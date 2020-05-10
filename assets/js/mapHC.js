63c7a234889bea2fb35c086de7d07059740dd150
antontj1 <antontj@astr.tohoku.ac.jp>
2020-05-10 23:04:01 +0900

    update

--- assets/js/mapHC.js
+++ assets/js/mapHC.js
@@ -1,34 +1,22 @@
 Highcharts.getJSON('https://raw.githubusercontent.com/antontj1/antontj1.github.io/master/assets/maps/telescopes.json?callback=?', function (data) {
+//Highcharts.getJSON('https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/world-population.json', function (data) {
 
-  // Prevent logarithmic errors in color calulcation
-  data.forEach(function (p) {
-    p.value = (p.value < 1 ? 0 : p.value);
-  });
-
-  // Initiate the chart
   Highcharts.mapChart('container', {
-
     chart: {
+      borderWidth: 1,
       map: 'custom/world'
     },
 
     title: {
-      text: 'Ground-based telescopes around the World'
+      text: 'Optic telescopes around the world'
+    },
+
+    subtitle: {
+      text: ''
     },
 
     legend: {
-      title: {
-        text: 'Number of telescopes',
-        style: {
-          color: ( // theme
-            Highcharts.defaultOptions &&
-            Highcharts.defaultOptions.legend &&
-            Highcharts.defaultOptions.legend.title &&
-            Highcharts.defaultOptions.legend.title.style &&
-            Highcharts.defaultOptions.legend.title.style.color
-          ) || 'black'
-        }
-      }
+      enabled: false
     },
 
     mapNavigation: {
@@ -38,36 +26,20 @@ Highcharts.getJSON('https://raw.githubusercontent.com/antontj1/antontj1.github.i
       }
     },
 
-    tooltip: {
-      backgroundColor: 'none',
-      borderWidth: 0,
-      shadow: false,
-      useHTML: true,
-      padding: 0,
-      pointFormat: '<span class="f32"><span class="flag {point.properties.hc-key}">' +
-        '</span></span> {point.name}<br>' +
-        '<span style="font-size:20px">{point.value}</span>',
-      positioner: function () {
-        return { x: 0, y: 250 };
-      }
-    },
-
-    colorAxis: {
-      min: 1,
-      max: 1000,
-      type: 'logarithmic'
-    },
-
     series: [{
-      data: data,
+      name: 'Countries',
+      color: '#E0E0E0',
+      enableMouseTracking: false
+    }, {
+      type: 'mapbubble',
+      name: 'updated by 2020',
       joinBy: ['iso-a3', 'code3'],
-      name: 'Details',
-      states: {
-        hover: {
-          color: '#a4edba'
-        }
+      data: data,
+      minSize: 4,
+      maxSize: '12%',
+      tooltip: {
+        pointFormat: '{point.properties.hc-a2}: {point.z} meter'
       }
     }]
   });
 });
-
