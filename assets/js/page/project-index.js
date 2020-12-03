389e417a4b60540c85844aefcd1769888f7ce3dc
antontj1 <antontj@astr.tohoku.ac.jp>
2020-12-03 17:33:39 +0900

    Update project-index.js

--- assets/js/page/project-index.js
+++ assets/js/page/project-index.js
@@ -48,7 +48,7 @@ $(function() {
                 },
             },
             bar: {
-                width: 8
+                width: 10
             },
             legend: {
                 show: true, //hide legend
